88e844c61a8ac0b8338384815c2bded01f61e029
Pippopad <pippopad2017@gmail.com>
2023-05-17 15:44:36 +0200

    Cambiato stile MainForm

---
+++ Mahazy/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Mahazy
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTitleBar = new System.Windows.Forms.Panel();
+            this.picBtnClose = new System.Windows.Forms.PictureBox();
+            this.picBtnMinimize = new System.Windows.Forms.PictureBox();
+            this.pnlTitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnMinimize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTitleBar
+            // 
+            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.pnlTitleBar.Controls.Add(this.picBtnMinimize);
+            this.pnlTitleBar.Controls.Add(this.picBtnClose);
+            this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitleBar.Name = "pnlTitleBar";
+            this.pnlTitleBar.Size = new System.Drawing.Size(791, 36);
+            this.pnlTitleBar.TabIndex = 0;
+            this.pnlTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseDown);
+            this.pnlTitleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseMove);
+            this.pnlTitleBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseUp);
+            // 
+            // picBtnClose
+            // 
+            this.picBtnClose.BackColor = System.Drawing.Color.LightGray;
+            this.picBtnClose.Location = new System.Drawing.Point(12, 10);
+            this.picBtnClose.Name = "picBtnClose";
+            this.picBtnClose.Size = new System.Drawing.Size(16, 16);
+            this.picBtnClose.TabIndex = 1;
+            this.picBtnClose.TabStop = false;
+            // 
+            // picBtnMinimize
+            // 
+            this.picBtnMinimize.BackColor = System.Drawing.Color.LightGray;
+            this.picBtnMinimize.Location = new System.Drawing.Point(34, 10);
+            this.picBtnMinimize.Name = "picBtnMinimize";
+            this.picBtnMinimize.Size = new System.Drawing.Size(16, 16);
+            this.picBtnMinimize.TabIndex = 2;
+            this.picBtnMinimize.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 397);
+            this.Controls.Add(this.pnlTitleBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mahazy";
+            this.pnlTitleBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnMinimize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitleBar;
+        private System.Windows.Forms.PictureBox picBtnClose;
+        private System.Windows.Forms.PictureBox picBtnMinimize;
+    }
+}
+
---
+++ Mahazy/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mahazy
+{
+    public partial class MainForm : Form
+    {
+        private Point startPoint;
+        private bool clicked;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.clicked = false;
+        }
+
+        private void pnlTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.startPoint = e.Location;
+            clicked = true;
+        }
+
+        private void pnlTitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clicked)
+            {
+                Point p1 = new Point(e.X, e.Y);
+                Point p2 = this.PointToScreen(p1);
+                Point p3 = new Point(p2.X - this.startPoint.X,
+                                     p2.Y - this.startPoint.Y);
+
+                this.Location = p3;
+            }
+        }
+
+        private void pnlTitleBar_MouseUp(object sender, MouseEventArgs e)
+        {
+            clicked = false;
+        }
+    }
+}
