6950d061d6966c05672221d168cd762307f72bf9
Pippopad <pippopad2017@gmail.com>
2023-05-23 23:24:53 +0200

    Iniziata StoreView & ProductComponent

---
+++ Mahazy/Components/ProductComponent.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Mahazy
+{
+    partial class ProductComponent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblSeller = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(3, 3);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(50, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.lblDescription.Location = new System.Drawing.Point(4, 36);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "Description";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(304, 9);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(41, 16);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "0.00 €";
+            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblSeller
+            // 
+            this.lblSeller.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeller.Location = new System.Drawing.Point(229, 73);
+            this.lblSeller.Name = "lblSeller";
+            this.lblSeller.Size = new System.Drawing.Size(125, 17);
+            this.lblSeller.TabIndex = 3;
+            this.lblSeller.Text = "By user";
+            this.lblSeller.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // ProductComponent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblSeller);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "ProductComponent";
+            this.Size = new System.Drawing.Size(359, 98);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblSeller;
+    }
+}
---
+++ Mahazy/Components/ProductComponent.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mahazy
+{
+    public partial class ProductComponent : UserControl
+    {
+        public string Title
+        {
+            get => lblTitle.Text;
+            set
+            {
+                if (value.Length > 30)
+                    lblTitle.Text = value.Substring(0, 30 - 3) + "...";
+                else
+                    lblTitle.Text = value;
+            }
+        }
+
+        public string Description
+        {
+            get => lblDescription.Text;
+            set
+            {
+                if (value.Length > 140)
+                    lblDescription.Text = value.Substring(0, 140 - 3) + "...";
+                else
+                    lblDescription.Text = value;
+            }
+        }
+
+        private double price = 0.00;
+        public double Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                UpdatePrice();
+            }
+        }
+
+        private string currency = "€";
+        public string Currency
+        {
+            get => currency;
+            set
+            {
+                Currency = value;
+                UpdatePrice();
+            }
+        }
+
+        public enum CurrencyPos
+        { LEFT, RIGHT }
+
+        private CurrencyPos currencyPosition = CurrencyPos.RIGHT;
+        public CurrencyPos CurrencyPosition
+        {
+            get => currencyPosition;
+            set
+            {
+                currencyPosition = value;
+                UpdatePrice();
+            }
+        }
+
+        private string seller = "By user";
+        public string Seller
+        {
+            get => seller;
+            set
+            {
+                if (value.Length > 14)
+                    lblSeller.Text = "By " + value.Substring(0, 14 - 3) + "...";
+                else
+                    lblSeller.Text = "By " + value;
+            }
+        }
+
+        public ProductComponent()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdatePrice()
+        {
+            if (CurrencyPosition == CurrencyPos.LEFT)
+                lblPrice.Text = $"{Currency} {Price.ToString("0.00")}";
+            else
+                lblPrice.Text = $"{Price.ToString("0.00")} {Currency}";
+        }
+    }
+}
